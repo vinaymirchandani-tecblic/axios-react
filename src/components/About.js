14c25bb33ccdb21e036aef7ca3694d4900065fb2
Vinay Mirchandani <vinaymirchandani.tecblic@gmail.com>
2023-02-04 18:43:36 +0530

    css grind

--- src/components/About.js
+++ src/components/About.js
@@ -2,26 +2,21 @@ import React from "react";
 
 export const About = () => {
   return (
-    <div className="container my-3">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in sapien
-      facilisis diam euismod semper nec sed sapien. Pellentesque iaculis
-      volutpat turpis nec varius. Nam porttitor at metus non consequat.
-      Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
-      cubilia curae; Vestibulum in justo neque. In egestas pellentesque massa,
-      id tincidunt est. Nam rutrum convallis felis sit amet ornare. Curabitur
-      blandit convallis nunc non tincidunt. Donec sed nibh maximus, rutrum urna
-      nec, lobortis erat. Etiam nec mi vestibulum, hendrerit lectus a, feugiat
-      sapien. Nulla nec tortor euismod, dignissim metus non, gravida odio. Etiam
-      sagittis, libero et eleifend faucibus, metus arcu maximus quam, ac
-      fermentum dolor justo sit amet enim. Class aptent taciti sociosqu ad
-      litora torquent per conubia nostra, per inceptos himenaeos. Morbi vel odio
-      finibus, convallis sapien id, dapibus justo. Nulla vitae ante purus.
-      Pellentesque habitant morbi tristique senectus et netus et malesuada fames
-      ac turpis egestas. Suspendisse quis velit in dolor lobortis hendrerit ut
-      eget risus. Nulla eleifend euismod nunc non pretium. Sed feugiat leo
-      sapien, et rhoncus massa pretium a. Curabitur aliquam justo eu felis
-      venenatis, et rhoncus velit aliquet. Ut lobortis diam nec lectus elementum
-      aliquam.
-    </div>
+    
+      <div className="container">
+        <h2 className="heading">  My Portfolio</h2>
+        <p className="subheading"> Focus on object</p>
+        <div className="box" id="box-1">"box-1"</div>
+        <div className="box" id="box-2">"box-2"</div>
+        <div className="box" id="box-3">"box-3"</div>
+        <div className="box" id="box-4">"box-4"</div>
+        <div className="box" id="box-5">"box-5"</div>
+        <div className="box" id="box-6">"box-6"</div>
+        <div className="box" id="box-7">"box-7"</div>
+        <div className="box" id="box-8">"box-8"</div>
+        <div className="box" id="box-9">"box-9"</div>
+        <div className="box" id="box-10">"box-10"</div>
+      </div>
+    
   );
 };
--- src/components/Entertainment.js
+++ src/components/Entertainment.js
@@ -23,32 +23,14 @@ export const Entertainment = () => {
   sapien, et rhoncus massa pretium a. Curabitur aliquam justo eu felis
   venenatis, et rhoncus velit aliquet. Ut lobortis diam nec lectus elementum
   aliquam. 
-  </div>;
+  </div>
   <div>
 
   lorem-ipsum.
-
-  Nam rutrum convallis felis sit amet ornare. Curabitur
-  blandit convallis nunc non tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in sapien
-  facilisis diam euismod semper nec sed sapien. Pellentesque iaculis
-  volutpat turpis nec varius. Nam porttitor at metus non consequat.
-  Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
-  cubilia curae; Vestibulum in justo neque. In egestas pellentesque massa,
-  id tincidunt est. Nam rutrum convallis felis sit amet ornare. Curabitur
-  blandit convallis nunc non tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in sapien
-  facilisis diam euismod semper nec sed sapien. Pellentesque iaculis
-  volutpat turpis nec varius. Nam porttitor at metus non consequat.
-  Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
-  cubilia curae; Vestibulum in justo neque. In egestas pellentesque massa,
-  id tincidunt est. Nam rutrum convallis felis sit amet ornare. Curabitur
-  blandit convallis nunc non tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in sapien
-  facilisis diam euismod semper nec sed sapien. Pellentesque iaculis
-  volutpat turpis nec varius. Nam porttitor at metus non consequat.
-  Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
-  cubilia curae; Vestibulum in justo neque. In egestas pellentesque massa,
-  id tincidunt est. Nam rutrum convallis felis sit amet ornare. Curabitur
-  blandit convallis nunc non tincidunt.
-  </div>
+div.row
+  
+  </div> 
+  .container
 
 <div className="gallery">
   <img src="./images/img1.png" alt="img1"></img>
@@ -61,6 +43,7 @@ export const Entertainment = () => {
 <img src="./images/img1.png" alt="img1"></img>
 
 </div>
+
 <div className="gallery">
 <img src="./images/img1.png" alt="img1"/>
 <div className="desc">Culpa fugiatIpsum excepteur in nostrud fugiat. elit officia magna est laborum dolor.Sunt tempor id id pariatur commodo exercitation laborum quis nisi in nostrud fugiat. </div>
